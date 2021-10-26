0a6633b7485bbd1ef19abd224e34057d257121f3
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2021-10-26 16:55:44 +0100

    update config

--- src/config.js
+++ src/config.js
@@ -5,12 +5,10 @@ export const customsListLocalStorageKey = "customs";
 // localStorage functions
 /// getter
 export function getLocal(key) {
-  // const d = localStorage.getItem(key);
-  // return d ? JSON.parse(d) : [];
-  return [];
+  const d = localStorage.getItem(key);
+  return d ? JSON.parse(d) : [];
 }
 /// setter
 export function setLocal(key, data) {
-  console.log(key, data);
-  // localStorage.getItem(key, JSON.stringify(data));
+  localStorage.getItem(key, JSON.stringify(data));
 }
