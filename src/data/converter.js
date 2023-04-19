ec329958e8e6747b40b732e8d0ba7d3b5d353249
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2023-04-19 06:16:33 +0000

    fix: channels stream url

--- src/data/converter.js
+++ src/data/converter.js
@@ -6,50 +6,60 @@ const { get } = require("axios");
 // variables
 const dir = __dirname + "/";
 const url = "https://iptv-org.github.io/api/channels.json";
+const streamsUrl = "https://iptv-org.github.io/api/streams.json";
 const dangerCategoriesList = ["xxx"];
 
 get(url)
   .then(({ data }) => {
     const items = [];
 
-    data.forEach((r) => {
-      // convert categories array ot objects to array of string
-      r.categories = Array.from(r.categories, (cat) => {
-        return cat.name;
+    // get streams
+    get(streamsUrl).then(({ data: streams }) => {
+      data.forEach((r) => {
+        // convert categories array ot objects to array of string
+        r.categories = Array.from(r.categories, (cat) => {
+          return cat.name;
+        });
+
+        // set default category for non-categorized
+        if (r.categories.length > 0) {
+          r.category = r.categories[0];
+        } else {
+          r.category = "Uncategorized";
+        }
+
+        // filter adults
+        if (!dangerCategoriesList.includes(lowerCase(r.category))) {
+          // update/set props
+          // r.id = kebabCase(lowerCase(r.name));
+
+          // remove non-used props
+          delete r.logo;
+          delete r.languages;
+          delete r.countries;
+          delete r.categories;
+          delete r.tvg;
+
+          // match streams with channels
+          r.url = streams.find((s) => s.channel == r.id)?.url || "";
+
+          // push it
+          items.push(r);
+        }
       });
 
-      // set default category for non-categorized
-      if (r.categories.length > 0) {
-        r.category = r.categories[0];
-      } else {
-        r.category = "Uncategorized";
-      }
+      // filter channels to make it uniq
+      const uniqData = uniqBy(items, (el) => {
+        return el.name;
+      }).filter((el) => {
+        // only non-emty channels url
+        return el.url != "";
+      });
 
-      // filter adults
-      if (!dangerCategoriesList.includes(lowerCase(r.category))) {
-        // update/set props
-        r.id = kebabCase(lowerCase(r.name));
-
-        // remove non-used props
-        delete r.logo;
-        delete r.languages;
-        delete r.countries;
-        delete r.categories;
-        delete r.tvg;
-
-        // push it
-        items.push(r);
-      }
-    });
-
-    // filter channels to make it uniq
-    const uniqData = uniqBy(items, (el) => {
-      return el.name;
-    });
-
-    writeJson(dir + "data.json", uniqData, () => {
-      // show message
-      console.log("Saved Channels Count:", uniqData.length);
+      writeJson(dir + "data.json", uniqData, () => {
+        // show message
+        console.log("Saved Channels Count:", uniqData.length);
+      });
     });
   })
   .catch((err) => {
